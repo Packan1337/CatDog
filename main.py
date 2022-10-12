0f2b9afa055d1dc8f5ff0a02db35cebd31732ed6
Packan <ariaxero@gmail.com>
2022-10-12 19:06:17 +0200

    fixed dogs function :)

--- main.py
+++ main.py
@@ -1,12 +1,5 @@
-from PIL import Image
 from modules import *
 
 
-"""image object"""
-img1 = Image.open('cats/cat.0.jpg')
-img2 = Image.open('cats/cat.1.jpg')
-
-convert_image(img1)
-convert_image(img2)
-
-print_numpy_list(list_of_numpydata)
+make_dogs(50)
+print_numpydata(dogs_numpydata)
--- modules.py
+++ modules.py
@@ -1,15 +1,34 @@
-
+import os
+import random
+from PIL import Image
 from numpy import asarray
 
-list_of_numpydata = []
+# Data containers.
+dogs_numpydata = []
+cats_numpydata = []
+mixed_numpydata = []
 
 
-def convert_image(image_to_convert):
+# Function that specifically adds new object to a list, used in other functions.
+def append_to_list(item_to_append, a_list: list):
+    a_list.append(item_to_append)
+
+
+# Convert image to certain template.
+def convert_and_store_image(image_to_convert):
     image_to_convert = image_to_convert.convert("L")
     image_to_convert = image_to_convert.resize((400, 400))
-    list_of_numpydata.append(asarray(image_to_convert))
+    append_to_list(asarray(image_to_convert), dogs_numpydata)
 
 
-def print_numpy_list(list_to_print):
+# Create a select amount of dog images and add their k-value to dogs_numpydata list.
+def make_dogs(amount_of_images_to_scan: int):
+    for _ in range(amount_of_images_to_scan):
+        img = Image.open("dogs/" + random.choice(os.listdir("dogs")))
+        convert_and_store_image(img)
+
+
+# Function to print entire list of k-values.
+def print_numpydata(list_to_print):
     for k in list_to_print:
         print(k[0, 0])
