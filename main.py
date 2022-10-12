7c642e89cc90167fe7e2fe381f727b1ddd5f7cbe
Packan <ariaxero@gmail.com>
2022-10-12 14:11:48 +0200

    yeet

---
+++ main.py
@@ -0,0 +1,12 @@
+from modules.py import *
+
+
+"""image object"""
+img1 = Image.open('cat.jpg')
+img2 = Image.open('cat.jpg')
+
+convert_image(img1)
+convert_image(img2)
+# Skriv ut pixel-värde i pixel 0,0
+for k in list_of_numpydata:
+    print(k[0, 0])
---
+++ modules.py
@@ -0,0 +1,10 @@
+from PIL import Image
+from numpy import asarray
+
+list_of_numpydata = []
+
+
+def convert_image(image_to_convert):
+    image_to_convert = image_to_convert.convert("L")
+    image_to_convert = image_to_convert.resize((400, 400))
+    list_of_numpydata.append(asarray(image_to_convert))
