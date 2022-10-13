1da3b71f9aa2d3395b5b2570ad43aef0b57b4c48
Packan <ariaxero@gmail.com>
2022-10-13 15:20:22 +0200

    fixed convert_and_store_image function

--- main.py
+++ main.py
@@ -2,5 +2,11 @@ import kivy
 from modules import *
 
 
-make_dogs(50)
+make_dogs(5)
 print_numpydata(dogs_numpydata)
+print("")
+make_cats(5)
+print_numpydata(cats_numpydata)
+print("")
+make_mix(5)
+print_numpydata(mix_numpydata)
--- modules.py
+++ modules.py
@@ -6,7 +6,7 @@ from numpy import asarray
 # Data containers.
 dogs_numpydata = []
 cats_numpydata = []
-mixed_numpydata = []
+mix_numpydata = []
 
 
 # Function that specifically adds new object to a list, used in other functions.
@@ -15,35 +15,39 @@ def append_to_list(item_to_append, a_list: list):
 
 
 # Convert image to certain template.
-def convert_and_store_image(image_to_convert):
+def convert_and_store_image(image_to_convert, animal):
     image_to_convert = image_to_convert.convert("L")
     image_to_convert = image_to_convert.resize((400, 400))
-    append_to_list(asarray(image_to_convert), dogs_numpydata)
+    if animal == "dog":
+        append_to_list(asarray(image_to_convert), dogs_numpydata)
+    elif animal == "cat":
+        append_to_list(asarray(image_to_convert), cats_numpydata)
+    elif animal == "mix":
+        append_to_list(asarray(image_to_convert), mix_numpydata)
 
 
 # Create a select amount of dog images and add their k-value to dogs_numpydata list.
 def make_dogs(amount_of_images_to_scan: int):
     for _ in range(amount_of_images_to_scan):
-        img = Image.open("dogs/" + random.choice(os.listdir("dogs")))
-        convert_and_store_image(img)
+        dog_img = Image.open("dogs/" + random.choice(os.listdir("dogs")))
+        convert_and_store_image(dog_img, "dog")
 
 
 # Create a select amount of cat images and add their k-value to dogs_numpydata list.
 def make_cats(amount_of_images_to_scan: int):
     for _ in range(amount_of_images_to_scan):
-        img = Image.open("cats/" + random.choice(os.listdir("cats")))
-        convert_and_store_image(img)
+        cat_img = Image.open("cats/" + random.choice(os.listdir("cats")))
+        convert_and_store_image(cat_img, "cat")
 
 
 # Create a select amount of both dog and cat images and add their k-value to dogs_numpydata list.
 def make_mix(amount_of_images_to_scan: int):
     for _ in range(amount_of_images_to_scan):
-        img = Image.open("mix/" + random.choice(os.listdir("mix")))
-        convert_and_store_image(img)
+        mix_img = Image.open("mix/" + random.choice(os.listdir("mix")))
+        convert_and_store_image(mix_img, "mix")
 
 
-    # Function to print entire list of k-values.
+# Function to print entire list of k-values.
 def print_numpydata(list_to_print):
     for k in list_to_print:
         print(k[0, 0])
-
