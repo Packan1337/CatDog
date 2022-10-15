5f6fd0e85099048ff8441b0ff46bd46edca6d482
Packan <ariaxero@gmail.com>
2022-10-15 11:12:45 +0200

    fixes

--- main.py
+++ main.py
@@ -1,12 +1,12 @@
-import kivy
+#import kivy
 from modules import *
 
 
-make_dogs(5)
+make_dogs(10)
 print_numpydata(dogs_numpydata)
 print("")
-make_cats(5)
+make_cats(10)
 print_numpydata(cats_numpydata)
 print("")
-make_mix(5)
+make_mix(0)
 print_numpydata(mix_numpydata)
