145c27fbc018545d23ba074380bb6f38b97659ff
Packan <ariaxero@gmail.com>
2022-10-21 09:57:12 +0200

    tamam

--- main.py
+++ main.py
@@ -5,16 +5,19 @@ import numpy as np
 from PIL import Image
 from numpy import asarray
 
+# Globala listor där vi lagrar data.
 dogs = []
 cats = []
 mix = []
 
 
+# Definerar Animal klassen som även innehåller metoder.
 class Animal:
     def __init__(self, animal_type, sum_of_pixels):
         self.animal_type = animal_type
         self.sum_of_pixels = sum_of_pixels
 
+    # Klassmetod som konvrterar och lagrar bilder i globala listor.
     @staticmethod
     def convert_and_store_image(image_to_convert, animal):
         image_to_convert = image_to_convert.convert("L")
@@ -40,18 +43,21 @@ class Animal:
             sum_of_mix_pixels = np.sum(asarray(image_to_convert))
             mix.append(Animal("The animal", sum_of_mix_pixels))
 
+    # Genererar en slumpvis hund
     @staticmethod
     def make_random_dog(amount_of_images_to_scan: int):
         for _ in range(amount_of_images_to_scan):
             dog_img = Image.open("dogs/" + random.choice(os.listdir("dogs")))
             return Animal.convert_and_store_image(dog_img, "test_dog")
 
+    # Genererar en slumpvis katt
     @staticmethod
     def make_random_cat(amount_of_images_to_scan: int):
         for _ in range(amount_of_images_to_scan):
             dog_img = Image.open("cats/" + random.choice(os.listdir("cats")))
             return Animal.convert_and_store_image(dog_img, "test_cat")
 
+    # Genererar en slumpvis djur, kan bli antingen hund eller katt.
     @staticmethod
     def make_random_animal():
         state = random.randrange(1, 3)
@@ -60,12 +66,14 @@ class Animal:
         elif state == 2:
             return Animal.make_random_cat(1)
 
+    # Genererar djur baserat på mix mappen, kan bli både hund och katter.
     @staticmethod
     def make_mix_animals(amount_to_make: int):
         for _ in range(amount_to_make):
             mix_img = Image.open("mix/" + random.choice(os.listdir("mix")))
             Animal.convert_and_store_image(mix_img, "mix")
 
+    # Genererar specifik mängd hundar.
     @staticmethod
     def make_dog():
         d_space = 0
@@ -74,6 +82,7 @@ class Animal:
             Animal.convert_and_store_image(dog_img, "dog")
             d_space += 1
 
+    # Genererar specifik mängd katter.
     @staticmethod
     def make_cat():
         d_space = 0
@@ -82,16 +91,19 @@ class Animal:
             Animal.convert_and_store_image(dog_img, "dog")
             d_space += 1
 
+    # Funktion som använder euclidean distance
     @staticmethod
     def euclidean_distance(x, y):
         return math.sqrt((float(x) - float(y)) ** 2)
 
 
+# Kallar funktioner för att generera data.
 mix_animal = Animal.make_random_animal()
 print(mix_animal.animal_type)
 Animal.make_mix_animals(20)
 
 
+# Funktion som kollar om en bild är hund eller katt.
 def check_animals(animals_to_check: list, test_animal):
     for animals in animals_to_check:
         if animals.sum_of_pixels > test_animal.sum_of_pixels:
@@ -102,4 +114,5 @@ def check_animals(animals_to_check: list, test_animal):
             print(f"{animals.animal_type} is 100% a {test_animal.animal_type}")
 
 
+# Kollar datan.
 check_animals(mix, mix_animal)
